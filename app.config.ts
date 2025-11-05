8e2c01dada0780c6b65a22a04401b4a78483be78
Yusaku0923 <nakayama8039@gmail.com>
2025-11-05 14:05:28 +0900

    バージョンダウン

--- app.config.ts
+++ app.config.ts
@@ -13,4 +13,31 @@ export default defineAppConfig({
     facebook: ''
   },
   // theme: 'neo-minimalist',
+  authors: [
+    {
+        username: 'hugo',
+        default: true,
+        name: 'Hugo Lassiège',
+        description:
+            'Main maintainer of Bloggrify',
+        avatar: '/images/avatar.jpeg',
+        socials: {
+            twitter: 'https://twitter.com/hugolassiege',
+            twitter_username: 'hugolassiege',
+            mastodon: 'https://piaille.fr/@hugolassiege',
+            youtube: 'https://youtube.com/@eventuallycoding',
+            linkedin: 'https://www.linkedin.com/in/hugolassiege/',
+            // facebook: 'https://facebook.com',
+            // instagram: 'https://instagram.com',
+            github: 'https://github.com/hlassiege',
+        },
+    },
+  ],
+
+  robots: [
+    {
+      UserAgent: '*',
+      Allow: ['/'],
+    },
+  ],
 })
