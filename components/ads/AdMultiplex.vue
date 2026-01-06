24722adf146a0924d3befca62dbebb7de59e6331
Yusaku0923 <nakayama8039@gmail.com>
2026-01-06 11:12:26 +0900

    広告エラー修正

--- components/ads/AdMultiplex.vue
+++ components/ads/AdMultiplex.vue
@@ -1,6 +1,6 @@
 <template>
   <ClientOnly>
-    <div class="article-ad my-8 flex justify-center w-full">
+    <div :key="adKey" class="article-ad my-8 flex justify-center w-full">
       <Adsense
         class="block max-w-full w-full"
         style="display: block; width: 100%; min-width: 320px"
@@ -13,6 +13,11 @@
 </template>
 
 <script setup lang="ts">
+const route = useRoute()
 const runtimeConfig = useRuntimeConfig()
 const adClient = runtimeConfig.public.adsenseClient || 'ca-pub-5447172429378250'
+
+// ルート変更時にユニークなキーを生成して広告を再初期化
+const adKey = computed(() => `ad-multiplex-${route.fullPath}-${Date.now()}`)
 </script>
+
